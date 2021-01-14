b8baf6facb152f640777a0bbb48cdebb71f86bf7
Marc Farias Jones <marc@liftedlogic.com>
2021-01-13 23:49:38 -0600

    added jsx sample

--- src/utils/languageSamples.js
+++ src/utils/languageSamples.js
@@ -107,6 +107,25 @@ const languageSamples = {
       </html>
             `,
     },
+    jsx: {
+        title: 'JSX',
+        code: `
+        import React from 'react';
+
+        class MyComponent extends React.Component {
+          render(){
+            return (
+              <React.Fragment>
+                <div>I am an element!</div>
+                <button>I am another element</button>
+              </React.Fragment>
+            );
+          }
+        }
+
+        export default MyComponent;
+      `,
+    },
     xml: {
         title: 'XML',
         code: `
